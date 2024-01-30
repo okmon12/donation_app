71322adcff2d034f135f28bb1b0089d73ef243f8
okmon12 <143723986+okmon12@users.noreply.github.com>
2024-01-30 23:22:38 +0900

    first commit

---
+++ app/models/address.rb
@@ -0,0 +1,5 @@
+class Address < ApplicationRecord
+  belongs_to :donation
+
+  
+end
---
+++ app/models/donation_address.rb
@@ -0,0 +1,15 @@
+class DonationAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :price, :user_id
+
+  with_options presence: true do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+end
+
+
+end
